67e3e0e986abecbe5580d3bf09b065abf0f49066
Rifhan Akram <rifhan.akram1@gmail.com>
2016-09-13 22:37:45 +0530

    - olark chat

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 
 require("babel-core/register");
 
-const app = require('express')();
+const express = require('express');
+const app = express();
 const retweet = require('./twitter_api/tweets');
 const scheduler = require('node-schedule');
 const morning = new scheduler.RecurrenceRule();
@@ -18,8 +19,8 @@ morning.hour = 3;
 morning.minute = 30;
 
 eve.dayOfWeek = [0,new scheduler.Range(0,6)];
-eve.hour = 12;
-eve.minute = 30;
+eve.hour = 20;
+eve.minute = 16;
 
 
 resultMorning = scheduler.scheduleJob(morning, ()=>{
@@ -34,14 +35,11 @@ scheduler.scheduleJob("*/15 * * * *",()=>{
     retweet.retweetBot();
 });
 
+app.use(express.static('public'));
 
 //app.use('/tweet',retweet());
 app.get('/',(req,res) => {
-    res.status(200).json({
-        "status" : "App Running",
-        "name" : "snowy the bot",
-        "description" : "This is a twitter bot"
-    });
+    res.sendFile('./public/index.html');
 })
 
 app.listen(process.env.PORT || 3000, () =>{
